fca680b0d7632b442a37a4cc5583daad5fa6fd3f
Rocky Duan <dementrock@gmail.com>
2012-06-06 15:02:52 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninjahelper::Application.config.secret_token = '804c924a190325d02eccbbd207da373575da1b93562204e95cac4198ee35d10c56fd97ca76e9a249252bbb825a1a8fb26012b5a09e5a06ce2213cf114eab4f37'

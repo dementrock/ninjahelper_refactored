b802527868561a40dd5e44d2f63b755418eee85d
Rocky Duan <dementrock@gmail.com>
2012-06-08 12:24:02 +0800

    extracted code for scheduling to a standalone module; implemented parsing for weekdays

--- app/models/courses.rb
+++ app/models/courses.rb
@@ -1,23 +1,22 @@
 class Courses < ActiveRecord::Base
-	attr_accessible :course_full_name, 
-									:course_title, 
-									:location, 
-									:time, 
-									:instructor, 
-									:status, 
-									:course_control_number, 
-									:units, 
-									:final_exam_group, 
-									:restrictions, 
-									:note, 
-									:enrollment, 
-									:department_name, 
-									:course_number, 
-									:course_dependency_type, 
-									:course_section_number, 
-									:course_type,
-									:enrollment_limit,
-									:enrolled,
-									:waitlist,
-									:avail_seats
+	attr_protected
+  def time_interval
+    return nil if self.time =~ /CANCELLED|TBA|UNSCHED|NO FACILITY/ || self.time.blank?
+    raw_day, raw_time_in_day = self.time.split
+    raw_day.gsub!(/SA/, "Sa")
+    day = []
+    Date::DAYNAMES.each do |dayname|
+      if raw_day.nil?
+        break
+      end
+      if raw_day.starts_with?(dayname[0..1])
+        day << dayname
+        raw_day = raw_day[2..-1]
+      elsif raw_day.starts_with?(dayname[0]) && (raw_day.length < 2 || raw_day[1].downcase != raw_day[1]) # to prevent Tuesday matched to "Th"
+        day << dayname
+        raw_day = raw_day[1..-1]
+      end
+    end
+    day
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,6 @@ module Ninjahelper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
   end
 end

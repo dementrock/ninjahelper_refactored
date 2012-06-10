3cdd398ef2ae12c43f3db97f1015b8aad8792ed6
Rocky Duan <dementrock@gmail.com>
2012-06-10 14:12:24 +0800

    separated berkeley_course and general course behavior

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'nokogiri'
 
 gem 'ampex'
 
+gem 'time_of_day'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/courses.rb
+++ app/models/courses.rb
@@ -1,22 +1,7 @@
+require 'berkeley_courses.rb'
+
 class Courses < ActiveRecord::Base
+  include BerkeleyCourses
 	attr_protected
-  def time_interval
-    return nil if self.time =~ /CANCELLED|TBA|UNSCHED|NO FACILITY/ || self.time.blank?
-    raw_day, raw_time_in_day = self.time.split
-    raw_day.gsub!(/SA/, "Sa")
-    day = []
-    Date::DAYNAMES.each do |dayname|
-      if raw_day.nil?
-        break
-      end
-      if raw_day.starts_with?(dayname[0..1])
-        day << dayname
-        raw_day = raw_day[2..-1]
-      elsif raw_day.starts_with?(dayname[0]) && (raw_day.length < 2 || raw_day[1].downcase != raw_day[1]) # to prevent Tuesday matched to "Th"
-        day << dayname
-        raw_day = raw_day[1..-1]
-      end
-    end
-    day
-  end
+
 end
